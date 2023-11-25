8fc51b8ffe42de5f7e47666ba6c35adfffa92719
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-11-25 22:34:19 +0200

    Initial Commit

---
+++ FridgeChallenge/FridgeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeChallengeApp.swift
+//  FridgeChallenge
+//
+//  Created by Quest76 on 25.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FridgeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
